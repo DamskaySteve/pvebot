5a205eaf46dd31edeba0cf71598871f7ce6a40b0
DamskaySteve <37937235+DamskaySteve@users.noreply.github.com>
2018-06-29 21:19:13 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("!")
+
+bot.on('ready', function() {
+    bot.user.setActivity("Bot pour Anti-Arnaque PVE");
+    console.log("Connecter");
+});
+
+bot.login(process.env.TOKEN)
+
+bot.on('message', message =>{
+    if (message.content ==="arnaque"){
+        message.reply("Merci de ton signalement !");
+        console.log("Commande reply salut faite !")
+    }
+	
+        if (message.content ==="Yo"){
+        message.reply("Yo tu vas bien :D");
+        console.log("Commande reply salut faite !")
+    }
+	
+	
+	if (message.content ==="Qui est la"){
+        message.reply("Moi chuis la mec !");
+        console.log("Commande reply salut faite !")
+    }
+	
+	if (message.content ==="!update"){
+        message.reply("Mise a jour en cours...");
+        console.log("Commande reply salut faite !")
+    }
+	
+	
+	
+});
+
+
+bot.on ("guildMemberAdd", member => {
+	member.guild.channels.find("name", "bienvenue").send('Un anti-arnaqueur est aparue ! Bienvenue sur Anti-Arnaque PVE !')
+
+});
